01ee81b6f2aa2fab023964a0a19aedf33b6eeed5
me <derbali.1987@gmail.com>
2019-05-24 09:25:15 +0200

    Create the part that let me add new posts

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from store import Post, PostStore
 
 app = Flask(__name__)
-@app.route("/")
-def home():
-    dummy_posts = [
+
+app.current_id = 3
+dummy_posts = [
         Post(id=1,
              photo_url='https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=50&w=50',
              name='Sara',
@@ -14,12 +14,30 @@ def home():
              name='John',
              body='Lorem Ipsum'),
     ]
-    post_store = PostStore()
-    post_store.add(dummy_posts[0])
-    post_store.add(dummy_posts[1])
-    # Get all Posts
-    posts = post_store.get_all()
 
+post_store = PostStore()
+post_store.add(dummy_posts[0])
+post_store.add(dummy_posts[1])
+# Get all Posts
+posts = post_store.get_all()
+
+@app.route("/")
+def home():
     return render_template("index.html",  posts=posts)
+
+@app.route("/posts/add", methods=["GET", "POST"])
+def post_add():
+    if (request.method == "POST"):
+        # Here where we wrote add posts commands
+        new_post = Post(id= app.current_id,
+                        photo_url=request.form["photo_url"],
+                        name=request.form["name"],
+                        body=request.form["body"])
+        post_store.add(new_post)
+        app.current_id += 1
+        return redirect(url_for("home"))
+    elif request.method == "GET":
+        return render_template("post-add.html")
+
 app.run()
 
